11cb866451b9ef26c5609512977cd3f9f637f275
N-Aryan <aryannarang110@gmail.com>
2025-09-06 00:58:54 +0530

    Create buggy_typo_2.py

---
+++ examples/buggy_typo_2.py
@@ -0,0 +1,6 @@
+# Intent: compare user input to 10
+x = int(input("Enter a number: "))
+if x = 10:                 # assignment (=) instead of comparison (==)
+    print("equal")
+else:
+    print("not equal")
